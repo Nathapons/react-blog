8976e0d0f3a913690abc2c957727be610e277b47
nuthapon <nuthaponm79@gmail.com>
2023-11-23 21:52:46 +0700

    feat: react prop types

--- src/components/BreadCrumb.jsx
+++ src/components/BreadCrumb.jsx
@@ -1,14 +1,17 @@
 import { Breadcrumb } from 'antd'
 import React from 'react'
+import PropTypes from 'prop-types';
 
-export default function BlogBreadCrumb(props) {
-    const dataArtr = props.data
-
+export default function BlogBreadCrumb({ dataArr }) {
     return (
         <Breadcrumb style={{paddingTop: "10px"}}>
-            {dataArtr.map((item, index) => {
+            {dataArr.map((item, index) => {
                 return <Breadcrumb.Item>{item}</Breadcrumb.Item>
             })}
         </Breadcrumb>
     )
+}
+
+BlogBreadCrumb.propTypes = {
+    dataArr: PropTypes.array
 }
